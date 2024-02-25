ef782f7c7a86c37a54f7f6274eda8cd71d393c00
javagurulv <javagurulv@gmail.com>
2024-02-25 22:45:55 +0200

    step_189 - person first/last name validation

---
+++ teacher/step_189/src/main/java/org/javaguru/travel/insurance/core/validations/person/PersonCodeFormatValidation.java
@@ -0,0 +1,42 @@
+package org.javaguru.travel.insurance.core.validations.person;
+
+import org.javaguru.travel.insurance.core.api.dto.AgreementDTO;
+import org.javaguru.travel.insurance.core.api.dto.PersonDTO;
+import org.javaguru.travel.insurance.core.api.dto.ValidationErrorDTO;
+import org.javaguru.travel.insurance.core.util.Placeholder;
+import org.javaguru.travel.insurance.core.validations.ValidationErrorFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+class PersonCodeFormatValidation extends TravelPersonFieldValidationImpl {
+
+    @Autowired private ValidationErrorFactory errorFactory;
+
+    @Override
+    public Optional<ValidationErrorDTO> validate(AgreementDTO agreement, PersonDTO person) {
+        if (!isPersonCodeNullOrBlank(person) && !isValidFormat(person)) {
+            Placeholder placeholder = new Placeholder("PERSONAL_CODE", person.getPersonCode());
+            return Optional.of(errorFactory.buildError("ERROR_CODE_21", List.of(placeholder)));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private boolean isPersonCodeNullOrBlank(PersonDTO person) {
+        return person.getPersonCode() == null || person.getPersonCode().isBlank();
+    }
+
+    private boolean isValidFormat(PersonDTO person) {
+        String patternString = "\\d{6}-\\d{5}$";
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(person.getPersonCode());
+        return matcher.matches();
+    }
+
+}
