c8e5658741a4debe5684ce5644dc75811ac1638d
javagurulv <javagurulv@gmail.com>
2024-04-10 13:04:03 +0300

    remove @Autowired from prod code

--- teacher/step_75/src/main/java/org/javaguru/travel/insurance/core/services/TravelCalculatePremiumServiceImpl.java
+++ teacher/step_75/src/main/java/org/javaguru/travel/insurance/core/services/TravelCalculatePremiumServiceImpl.java
@@ -6,7 +6,6 @@ import org.javaguru.travel.insurance.dto.RiskPremium;
 import org.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
 import org.javaguru.travel.insurance.dto.ValidationError;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -16,8 +15,14 @@ import java.util.stream.Collectors;
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
 
-    @Autowired private TravelCalculatePremiumRequestValidator requestValidator;
-    @Autowired private TravelPremiumUnderwriting premiumUnderwriting;
+    private final TravelCalculatePremiumRequestValidator requestValidator;
+    private final TravelPremiumUnderwriting premiumUnderwriting;
+
+    TravelCalculatePremiumServiceImpl(TravelCalculatePremiumRequestValidator requestValidator,
+                                      TravelPremiumUnderwriting premiumUnderwriting) {
+        this.requestValidator = requestValidator;
+        this.premiumUnderwriting = premiumUnderwriting;
+    }
 
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
