8d402eab6ac4568dfbd97377dc75a8cbc7330a1c
javagurulv <javagurulv@gmail.com>
2024-04-10 21:38:13 +0300

    remove @Autowired from prod code

--- teacher/step_101/src/main/java/org/javaguru/travel/insurance/core/validations/DateFromLessThenDateToValidation.java
+++ teacher/step_101/src/main/java/org/javaguru/travel/insurance/core/validations/DateFromLessThenDateToValidation.java
@@ -2,7 +2,6 @@ package org.javaguru.travel.insurance.core.validations;
 
 import org.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.dto.ValidationError;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -11,7 +10,11 @@ import java.util.Optional;
 @Component
 class DateFromLessThenDateToValidation extends TravelRequestValidationImpl {
 
-    @Autowired private ValidationErrorFactory errorFactory;
+    private final ValidationErrorFactory errorFactory;
+
+    DateFromLessThenDateToValidation(ValidationErrorFactory errorFactory) {
+        this.errorFactory = errorFactory;
+    }
 
     @Override
     public Optional<ValidationError> validate(TravelCalculatePremiumRequest request) {
