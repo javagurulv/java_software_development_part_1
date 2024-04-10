76e9cf0a93764d2af1e2133229c13202abaa95cc
javagurulv <javagurulv@gmail.com>
2024-04-10 21:14:40 +0300

    remove @Autowired from prod code

--- teacher/step_85/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
+++ teacher/step_85/src/main/java/org/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
@@ -4,7 +4,6 @@ import com.google.common.base.Stopwatch;
 import org.javaguru.travel.insurance.core.services.TravelCalculatePremiumService;
 import org.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/insurance/travel")
 public class TravelCalculatePremiumController {
 
-	@Autowired private TravelCalculatePremiumRequestLogger requestLogger;
-	@Autowired private TravelCalculatePremiumResponseLogger responseLogger;
-	@Autowired private TravelCalculatePremiumRequestExecutionTimeLogger executionTimeLogger;
-	@Autowired private TravelCalculatePremiumService calculatePremiumService;
+	private final TravelCalculatePremiumRequestLogger requestLogger;
+	private final TravelCalculatePremiumResponseLogger responseLogger;
+	private final TravelCalculatePremiumRequestExecutionTimeLogger executionTimeLogger;
+	private final TravelCalculatePremiumService calculatePremiumService;
+
+	TravelCalculatePremiumController(TravelCalculatePremiumRequestLogger requestLogger,
+									 TravelCalculatePremiumResponseLogger responseLogger,
+									 TravelCalculatePremiumRequestExecutionTimeLogger executionTimeLogger,
+									 TravelCalculatePremiumService calculatePremiumService) {
+		this.requestLogger = requestLogger;
+		this.responseLogger = responseLogger;
+		this.executionTimeLogger = executionTimeLogger;
+		this.calculatePremiumService = calculatePremiumService;
+	}
 
 	@PostMapping(path = "/",
 			consumes = "application/json",
