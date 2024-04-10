c305730478dd37b1edaf4a4eabda8e2a77881e1b
javagurulv <javagurulv@gmail.com>
2024-04-10 21:29:59 +0300

    remove @Autowired from prod code

--- teacher/step_100/src/main/java/org/javaguru/travel/insurance/core/underwriting/calculators/medical/RiskLimitLevelCalculator.java
+++ teacher/step_100/src/main/java/org/javaguru/travel/insurance/core/underwriting/calculators/medical/RiskLimitLevelCalculator.java
@@ -3,7 +3,6 @@ package org.javaguru.travel.insurance.core.underwriting.calculators.medical;
 import org.javaguru.travel.insurance.core.domain.MedicalRiskLimitLevel;
 import org.javaguru.travel.insurance.core.repositories.MedicalRiskLimitLevelRepository;
 import org.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +14,11 @@ class RiskLimitLevelCalculator {
     @Value( "${medical.risk.limit.level.enabled:false}" )
     private Boolean medicalRiskLimitLevelEnabled;
 
-    @Autowired private MedicalRiskLimitLevelRepository riskLimitLevelRepository;
+    private final MedicalRiskLimitLevelRepository riskLimitLevelRepository;
+
+    RiskLimitLevelCalculator(MedicalRiskLimitLevelRepository riskLimitLevelRepository) {
+        this.riskLimitLevelRepository = riskLimitLevelRepository;
+    }
 
     BigDecimal calculate(TravelCalculatePremiumRequest request) {
         return medicalRiskLimitLevelEnabled
