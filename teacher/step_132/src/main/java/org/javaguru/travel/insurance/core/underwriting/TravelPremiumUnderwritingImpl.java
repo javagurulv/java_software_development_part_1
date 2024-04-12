9f9d16714fa7e1b7483ad43653d9d14f74c9c156
javagurulv <javagurulv@gmail.com>
2024-04-12 17:50:05 +0300

    @RequiredArgsConstructor(access = AccessLevel.PACKAGE)

--- teacher/step_132/src/main/java/org/javaguru/travel/insurance/core/underwriting/TravelPremiumUnderwritingImpl.java
+++ teacher/step_132/src/main/java/org/javaguru/travel/insurance/core/underwriting/TravelPremiumUnderwritingImpl.java
@@ -1,5 +1,7 @@
 package org.javaguru.travel.insurance.core.underwriting;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import org.javaguru.travel.insurance.core.api.dto.AgreementDTO;
 import org.javaguru.travel.insurance.core.api.dto.PersonDTO;
 import org.javaguru.travel.insurance.core.api.dto.RiskDTO;
@@ -10,13 +12,10 @@ import java.math.BigDecimal;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class TravelPremiumUnderwritingImpl implements TravelPremiumUnderwriting {
 
     private final SelectedRisksPremiumCalculator selectedRisksPremiumCalculator;
-
-    TravelPremiumUnderwritingImpl(SelectedRisksPremiumCalculator selectedRisksPremiumCalculator) {
-        this.selectedRisksPremiumCalculator = selectedRisksPremiumCalculator;
-    }
 
     @Override
     public TravelPremiumCalculationResult calculatePremium(AgreementDTO agreementDTO, PersonDTO person) {
